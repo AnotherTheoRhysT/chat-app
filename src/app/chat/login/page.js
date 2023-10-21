361fcfaaf4c736e190d0c3c5a386c9030d03fbb2
AnotherTheoRhysT <rhyscabonita@gmail.com>
2023-10-21 23:43:33 +0800

    Removed Shadcn form template (it was causing bugs)

--- src/app/chat/login/page.js
+++ src/app/chat/login/page.js
@@ -1,125 +1,91 @@
 "use client"
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
+import React, { useContext, useEffect, useState } from 'react'
 
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { useContext, useState } from "react"
-import axios from "axios"
-import { useRouter } from "next/navigation"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { ReloadIcon } from '@radix-ui/react-icons'
+
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import axios from 'axios'
 import { v4 as uuidv4 } from "uuid"
-import Cookies from "universal-cookie"
-import { AuthContext } from "@/components/providers/AuthProvider"
-import { ReloadIcon } from "@radix-ui/react-icons"
+import Cookies from 'universal-cookie'
+import { AuthContext } from '@/components/providers/AuthProvider'
+import { useRouter } from 'next/navigation'
 
-axios.defaults.withCredentials = true
-
-const cookies = new Cookies()
-
-const LoginSchema = z.object({
+const loginSchema = z.object({
   email: z.string().email({ message: "Invalid email" }),
   password: z.string()
 }).required()
 
-/**
- * TODO: Remove ZOD, Use React-Hook-Form, extract client components
- */
+const cookies = new Cookies()
+
 const Page = () => {
-  const [submitting, setSubmitting] = useState(false)
-  const form = useForm({
-    resolver: zodResolver(LoginSchema),
-  })
-
+	const { register, handleSubmit } = useForm({
+		resolver: zodResolver(loginSchema)
+	})
+	const [submitting, setSubmitting] = useState(false)
+  const [auth, setAuth] = useContext(AuthContext)
   const router = useRouter()
-  
-  if (cookies.get('bearer-token')) router.push('/chat')
 
-	// Handle form submission
-  const onSubmit = (formData) => {
-    setSubmitting(true)
-    console.log('submitting...')
+	useEffect(() => {
 		axios.get(`${process.env.SERVER_URL}/sanctum/csrf-cookie`)
 			.then(res => {
 				console.log("Got sanctum", res)
-				axios.post(`${process.env.SERVER_URL}/login`, {
-					email: formData.email,
-					password: formData.password,
-					token_name: uuidv4(),
-				})
-				.then(res => {
-					console.log("logged in", res)
-					setAuth(res.data.user)
-					const bearerToken = `Bearer ${res.data.token}`
-					cookies.set('bearer-token', bearerToken, {path: '/'})
-					axios.defaults.headers.common['Authorization'] = bearerToken
-          setSubmitting(false)
-					router.push('/chat')
-				})
-				.catch(err => {
-					console.log("log in error", err)
-				})
 			})
 			.catch(err => {
 				console.log("Sanctum error", err)
 			})
-  }
+	}, [])
 
-  const [auth, setAuth] = useContext(AuthContext)
+  if (cookies.get('bearer-token')) {
+		router.push('/chat')
+	}
 
-  return (
-    <Form {...form}	>
-      <main className='grid place-items-center'>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="w-1/3 space-y-6">
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-            />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input {...field} type="password"/>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-            />
-          {
-            (submitting)
-            ?
-            <Button disabled>
-              <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-              Submitting
-            </Button>
-            :
-            <Button type="submit">Submit</Button>
-          }
-        </form>
-      </main>
-    </Form>
-  )
+
+	const onSubmit = (formData) => {
+		setSubmitting(true)
+		console.log(formData)
+		axios.post(`${process.env.SERVER_URL}/login`, {
+			email: formData.email,
+			password: formData.password,
+			token_name: uuidv4(),
+		})
+		.then(res => {
+			console.log("logged in", res)
+			setAuth(res.data.user)
+			const bearerToken = `Bearer ${res.data.token}`
+			cookies.set('bearer-token', bearerToken, {path: '/'})
+			axios.defaults.headers.common['Authorization'] = bearerToken
+      setSubmitting(false)
+			router.push('/chat')
+		})
+		.catch(err => {
+			console.log("log in error", err)
+		})
+	}
+
+	return (
+		<main className='grid place-items-center'>
+			<form onSubmit={handleSubmit(onSubmit)} className='min-w-[30rem]'>
+				<Input className="mb-2" placeholder="Email" type="text" {...register("email")}/>
+				<Input className="mb-2" placeholder="Password" type="password" {...register("password")} />
+				<div className='flex flex-row-reverse'>
+					{ submitting ?
+						<Button disabled>
+							<ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+							Signing in
+						</Button>
+						:
+						<Button type="submit">Sign in</Button>
+					}
+				</div>
+			</form>
+		</main>
+	)
 }
 
 export default Page
