d5f890499e6da76e49db43434b2109bc5e553731
AnotherTheoRhysT <rhyscabonita@gmail.com>
2023-10-22 07:36:50 +0800

    Data fetching for conversation list

---
+++ src/app/chat/_data/useConversations.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import useSWR from 'swr'
+
+const fetcher = url => axios.get(url).then(res => res.data)
+
+const useConversations = () => {
+	const { data, error, isLoading } = useSWR(`${process.env.SERVER_URL}/api/chat/conversation/`, fetcher)
+	return {
+		data, error, isLoading
+	}
+}
+
+export default useConversations
