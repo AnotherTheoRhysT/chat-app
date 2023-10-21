29214f0aaae4b9dd7df9249eca6a4efba467bb92
AnotherTheoRhysT <rhyscabonita@gmail.com>
2023-10-21 23:44:27 +0800

    chat index page

---
+++ src/app/chat/page.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import UserList from "./UserList"
+
+
+const Page = () => {
+	// const users = () => {
+	// 	const res = fetch(`${process.env.SERVER_URL}/api/users`)
+	// 	if (!res.ok) throw new Error("Failed to fetch data")
+	// 	return res.json()
+	// }
+	// console.log(users())
+	return (
+		<>
+			Test
+			{/* <h1>Chat App</h1> */}
+			{/* <UserList/> */}
+		</>
+	)
+}
+
+export default Page
