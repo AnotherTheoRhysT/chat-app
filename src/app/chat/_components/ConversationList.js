cdd3150d8f5b53e4197c31a66e2e1b6a9b71f19f
AnotherTheoRhysT <rhyscabonita@gmail.com>
2023-10-22 07:35:38 +0800

    Added conversationList

---
+++ src/app/chat/_components/ConversationList.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+import useConversations from '../_data/useConversations'
+import ConversationListLoader from './ConversationListLoader'
+
+const ConversationList = ({currentConvo, setCurrentConvo}) => {
+	const {data, error, isLoading} = useConversations()
+
+	if (error) {
+		console.log('Conversations Error', error)
+		return 'Error in fetching conversations'
+	}
+	if (isLoading) return <ConversationListLoader />
+	if (data?.conversations)
+		return (
+			<ul>
+				{data.conversations.map(conversation => {
+					const active = conversation.id === currentConvo?.id
+					return (
+						<div key={conversation.id} className={`py-2 px-1 hover:bg-slate-200 ${active ? 'bg-slate-200' : ''}`} onClick={() => {setCurrentConvo(conversation)}}>
+							<li key={conversation.id}>{conversation.conversation_name ?? 'No Name'}</li>
+						</div>
+					)
+				})}
+			</ul>
+		)
+}
+
+export default ConversationList
